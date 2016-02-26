9157636bae4bf54dbfd5f8123e0fe2d5ddaf3d5e
Anthony Leon <anthonyleon@me.com>
2016-02-25 19:18:57 -0500

    parts local database created and populated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+parts_database = [
+  ["MAIN ENGINE CONTROL", "8063-215", "", 1, "WOODWARD", 32560.00],
+  ["SHAFT - HPT REAR", "1864M91P02", "", 1, "CFMI", 98010.00],
+  ["COVER - SPINNER CONE", "335-106-402-0", "", 1, "CFMI", 5257.00],
+  ["SHAFT - HPT REAR", "1864M91P02", "", 1, "CFMI", 539200.00]
+]
+
+parts_database.each do |description, partnum, serialnum, minqty, manufacturer, mfgprice|
+  Part.create(Description: description, PartNum: partnum, SerialNum: serialnum, MinQty: minqty, Manufacturer: manufacturer, MfgPrice: mfgprice)
+end
