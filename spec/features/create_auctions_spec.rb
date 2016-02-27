60638a7dbacf1b5c534b72c0f59dd79f5887ad7b
Nicholas Rodriguez <nrodriguez3179@gmail.com>
2016-02-27 18:32:04 -0500

    Added rspec feature tests

--- spec/features/create_auctions_spec.rb
+++ spec/features/create_auctions_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.feature "CreateAuctions", type: :feature do
   # pending "add some scenarios (or delete) #{__FILE__}"
-  before(:all) do
-    @auction = Auction.create(name:"Landing Gear" , description:"Auction for Landing Gear parts" )
+  scenario "a bid is created within an auction" do
+  # before(:all) do
+  bid = Bid.all
+  initial_count = Auction.bid.length
+  visit new_auction_bid_path
+
+  fill_in 'Amount', with: 10000
+
+  click_button 'submit'
+
+  after_count = Auction.bid.length
+
+  expect(after_count).to eq initial_count
   end
-end 
+end
