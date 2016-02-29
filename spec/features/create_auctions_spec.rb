1ae2dd03b91352b2c9a0981c71cdc52278327088
san3091 <san3091@gmail.com>
2016-02-29 13:30:41 -0500

    work on feature tests

--- spec/features/create_auctions_spec.rb
+++ spec/features/create_auctions_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.feature "CreateAuctions", type: :feature do
   # pending "add some scenarios (or delete) #{__FILE__}"
   scenario "a bid is created within an auction" do
-  # before(:all) do
-  bid = Bid.all
-  initial_count = Auction.bid.length
-  visit new_auction_bid_path
+    @auction = Auction.create(name: "Need an engine thing", description: "I need it quick")
+    # before(:all) do
+    initial_count = @auction.bids.length
+    visit new_auction_bid_path(@auction)
 
-  fill_in 'Amount', with: 10000
+    fill_in 'Amount', with: 10000
 
-  click_button 'submit'
+    click_button 'submit'
 
-  after_count = Auction.bid.length
+    after_count = @auction.bids.length
 
-  expect(after_count).to eq initial_count
+    expect(after_count).to be > initial_count
   end
 end
